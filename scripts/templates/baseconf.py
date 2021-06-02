0e8271d3a8557a3f9a4550585017c3b57dea557d
Martin Liska <mliska@suse.cz>
2021-06-02 09:07:55 +0200

    Remove for now html_static_path.

--- scripts/templates/baseconf.py
+++ scripts/templates/baseconf.py
@@ -73,11 +73,6 @@ exclude_patterns = ['_build']
 #
 html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.

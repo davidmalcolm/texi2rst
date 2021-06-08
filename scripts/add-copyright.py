c35d6023273393932285ae510cf15b1604fae600
Martin Liska <mliska@suse.cz>
2021-06-08 09:56:19 +0200

    Add copyright at the end.

---
+++ scripts/add-copyright.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Add copyright')
+parser.add_argument('rst_dir', help='Directory with RST files')
+args = parser.parse_args()
+
+for root, _, files in os.walk(args.rst_dir):
+    for f in files:
+        if f.endswith('.rst'):
+            full = os.path.join(root, f)
+            data = open(full).read()
+            with open(full, 'w') as out:
+                out.write('..\n')
+                out.write('  Copyright 1988-2021 Free Software Foundation, Inc.\n')
+                out.write('  This is part of the GCC manual.\n')
+                out.write('  For copying conditions, see the GPL license file\n')
+                out.write('\n')
+                out.write(data)

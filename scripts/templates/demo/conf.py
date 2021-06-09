a7d05515f83845050d66c6c6451b9e9df154ffca
Martin Liska <mliska@suse.cz>
2021-06-09 11:26:43 +0200

    Add demo project.

---
+++ scripts/templates/demo/conf.py
@@ -0,0 +1,12 @@
+# Configuration file for the Sphinx documentation builder.
+
+import sys
+sys.path.append('..')
+
+from baseconf import *
+
+project = 'Demo project'
+copyright = '2001-2021 Free Software Foundation, Inc.'
+authors = 'Martin Liska'
+
+tags.add('demo')
---
+++ scripts/templates/demo/index.rst
@@ -0,0 +1,7 @@
+Welcome to demo documentation!
+==============================
+
+Introduction
+============
+
+I'm a demo documentation page.

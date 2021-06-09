a61ec4e57d01c3e0086aef1f39a7d6cd0d18e8f8
Martin Liska <mliska@suse.cz>
2021-06-09 11:45:47 +0200

    demo: add latex target.

--- scripts/templates/demo/conf.py
+++ scripts/templates/demo/conf.py
@@ -9,4 +9,8 @@ project = 'Demo project'
 copyright = '2001-2021 Free Software Foundation, Inc.'
 authors = 'Martin Liska'
 
+texinfo_documents = [
+  ('index', 'demo', project, authors, None, None, None, True)
+]
+
 tags.add('demo')

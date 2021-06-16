09db54ab8d2fef88ef2b3c0c1487bb0d8babca8a
Martin Liska <mliska@suse.cz>
2021-06-16 14:41:10 +0200

    Add more split points.

--- texi2rst.py
+++ texi2rst.py
@@ -302,7 +302,9 @@ def split(tree):
                          or parent_text == 'Built-in Functions Specific to Particular Target Machines')):
                 return True
             elif (args and 'gccint.xml' in args.xml_file and element.kind == 'subsection'
-                    and parent_text == 'The gcc Subdirectory'):
+                    and parent_text in ('The gcc Subdirectory', 'Directives used within DejaGnu tests',
+                                        'Tuple specific accessors', 'Operand Constraints'
+                                        'Stack Layout and Calling Conventions')):
                 return True
             elif text == 'Contributors to GCC':
                 return True

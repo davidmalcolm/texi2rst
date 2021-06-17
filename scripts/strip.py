3895a763a6e05e9d56b600178b558afad2724e81
Martin Liska <mliska@suse.cz>
2021-06-17 14:44:49 +0200

    Update conf.py files.

--- scripts/strip.py
+++ scripts/strip.py
@@ -5,21 +5,29 @@ import sys
 
 for root, _, files in os.walk(sys.argv[1]):
     for f in files:
+        full = os.path.join(root, f)
+        parts = len(root.split('/'))
+        up = '../' * (parts - 1)
         if f.endswith('.rst'):
-            full = os.path.join(root, f)
             lines = open(full).read().strip().splitlines()
             newlines = []
             for line in lines:
                 line = line.rstrip()
                 prefix = '.. include:: ../share/'
                 if line.startswith(prefix):
-                    parts = len(root.split('/'))
-                    up = '../' * (parts - 1)
                     newline = f'.. include:: {up}doc/{line[len(prefix):]}'
                     newlines.append(newline)
-                    print(root, line, newline)
                 else:
                     newlines.append(line)
 
+            with open(full, 'w') as f:
+                f.write('\n'.join(newlines))
+        elif f == 'conf.py':
+            lines = open(full).read().splitlines()
+            newlines = []
+            for line in lines:
+                if line.startswith('sys.path.append'):
+                    line = f"sys.path.append('{up}/doc')"
+                newlines.append(line)
             with open(full, 'w') as f:
                 f.write('\n'.join(newlines))

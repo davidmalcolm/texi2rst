fe10272fb72e37c1f8ec205054c306e60db55c09
Martin Liska <mliska@suse.cz>
2021-06-17 22:23:16 +0200

    Fix include fix.

--- scripts/strip.py
+++ scripts/strip.py
@@ -6,19 +6,24 @@ import sys
 for root, _, files in os.walk(sys.argv[1]):
     for f in files:
         full = os.path.join(root, f)
-        parts = len(root.split('/'))
-        up = '../' * (parts - 1)
+        parts = root.split('/')
+        up2 = '../' * (len(parts) - 1)
+        print(full)
+        while parts:
+            if os.path.exists(os.path.join(*parts, 'conf.py')):
+                break
+            parts = parts[:-1]
+
+        up1 = '../' * (len(parts) - 2) if parts else None
+
         if f.endswith('.rst'):
             lines = open(full).read().strip().splitlines()
             newlines = []
             for line in lines:
                 line = line.rstrip()
-                prefix = '.. include:: ../share/'
-                if line.startswith(prefix):
-                    newline = f'.. include:: {up}doc/{line[len(prefix):]}'
-                    newlines.append(newline)
-                else:
-                    newlines.append(line)
+                if '.. include:: ' in line and 'share' in line:
+                    line = line.replace('share', f'{up1}doc')
+                newlines.append(line)
 
             with open(full, 'w') as f:
                 f.write('\n'.join(newlines))
@@ -27,7 +32,7 @@ for root, _, files in os.walk(sys.argv[1]):
             newlines = []
             for line in lines:
                 if line.startswith('sys.path.append'):
-                    line = f"sys.path.append('{up}/doc')"
+                    line = f"sys.path.append('{up2}/doc')"
                 newlines.append(line)
             with open(full, 'w') as f:
                 f.write('\n'.join(newlines))

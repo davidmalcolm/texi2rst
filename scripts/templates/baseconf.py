6f2e1f1487fb6ba158daea1229cbd875d047b828
Martin Liska <mliska@suse.cz>
2021-08-18 15:58:25 +0200

    Fix mappings.

--- scripts/templates/baseconf.py
+++ scripts/templates/baseconf.py
@@ -117,4 +117,4 @@ intersphinx_mapping = {}
 for manual in ['cpp', 'cppinternals', 'fortran', 'gcc', 'gccgo', 'gccint', 'gdc',
                'gfc-internals', 'gnat-style', 'gnat_rm', 'gnat_ugn', 'install',
                'libgccjit', 'libgomp', 'libiberty', 'libitm', 'libquadmath']:
-    intersphinx_mapping[manual] = ('https://splichal.eu/scripts/sphinx/{manual}/_build/html/', None)
+    intersphinx_mapping[manual] = (f'https://splichal.eu/scripts/sphinx/{manual}/_build/html/', None)

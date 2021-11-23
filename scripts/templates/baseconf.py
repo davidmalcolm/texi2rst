41b39489c3518f55aa5cb71c799561497f73efd8
Martin Liska <mliska@suse.cz>
2021-11-23 14:26:38 +0100

    Remove intersphinx target.

--- scripts/templates/baseconf.py
+++ scripts/templates/baseconf.py
@@ -135,7 +135,7 @@ texinfo_elements = { 'preamble': """
 
 # Cross manual reference mapping
 intersphinx_mapping = {}
-for manual in ['cpp', 'cppinternals', 'fortran', 'gcc', 'gccgo', 'gccint', 'gdc',
+for manual in ['cpp', 'cppinternals', 'gfortran', 'gcc', 'gccgo', 'gccint', 'gdc',
                'gfc-internals', 'gnat-style', 'gnat_rm', 'gnat_ugn', 'install',
                'libgccjit', 'libgomp', 'libiberty', 'libitm', 'libquadmath']:
     intersphinx_mapping[manual] = (f'https://splichal.eu/scripts/sphinx/{manual}/_build/html/', None)

f99c2125e0457dd62b7567445a35db93ab17fd33
Martin Liska <mliska@suse.cz>
2021-12-16 15:26:24 +0100

    Add more gols.

--- scripts/fix-option-tables.py
+++ scripts/fix-option-tables.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser(description='Fix option tables')
 parser.add_argument('rst_dir', help='Directory with RST files')
 args = parser.parse_args()
 
-FILES = ['gcc-command-options/option-summary.rst']
+FILES = ['gcc-command-options/option-summary.rst', 'gcc-command-options/options-that-control-optimization.rst']
 
 for root, _, files in os.walk(args.rst_dir):
     for f in files:
@@ -18,6 +18,7 @@ for root, _, files in os.walk(args.rst_dir):
                 with open(full, 'w') as out:
                     options = []
                     for i, line in enumerate(lines):
-                        if line.startswith('  :option:') and lines[i + 1].strip():
+                        if (line.startswith('  :option:') and lines[i + 1].strip()
+                                and '|gol|' not in line and line.rstrip().endswith('`')):
                             line = line.rstrip() + ' |gol|'
                         out.write(line + '\n')

1056d7735b066e824b656a8437cc4bed33dceb5c
Martin Liska <mliska@suse.cz>
2021-12-16 14:53:47 +0100

    Emit |gol| directives.

---
+++ scripts/fix-option-tables.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Fix option tables')
+parser.add_argument('rst_dir', help='Directory with RST files')
+args = parser.parse_args()
+
+FILES = ['gcc-command-options/option-summary.rst']
+
+for root, _, files in os.walk(args.rst_dir):
+    for f in files:
+        if f.endswith('.rst'):
+            full = os.path.join(root, f)
+            if any(full.endswith(x) for x in FILES):
+                lines = open(full).read().splitlines()
+                with open(full, 'w') as out:
+                    options = []
+                    for i, line in enumerate(lines):
+                        if line.startswith('  :option:') and lines[i + 1].strip():
+                            line = line.rstrip() + ' |gol|'
+                        out.write(line + '\n')
+
